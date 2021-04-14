75f0c1cda449bfd8286724731a56d369705d7beb
StanislavSemenov <78257261+StanislavSemenov@users.noreply.github.com>
2021-04-15 02:49:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './Styles.css';
 import ArrowsDefinition from './components/arrowsDefinition';
+import { useState } from 'react';
 
 const App = () => {
+  let size = 3
+  let arrowQuantity = 10
+  const [s, setS] = useState(false)
 
   return (
     <div >
-      {ArrowsDefinition(3, 10)}
+      {!s &&
+        <button className='start' onClick={() => setS(s => !s)}>
+          Начать игру
+        </button>
+      }
+      {s && <ArrowsDefinition size={size} arrowQuantity={arrowQuantity} />}
     </div >
   );
 }

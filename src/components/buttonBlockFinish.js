db996b77f6d9030c6b6fd3c58122257ea00d59bd
StanislavSemenov <78257261+StanislavSemenov@users.noreply.github.com>
2021-04-15 02:51:37 +0300

    Update buttonBlockFinish.js

--- src/components/buttonBlockFinish.js
+++ src/components/buttonBlockFinish.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import '../Styles.css'
+import { useState } from 'react';
 
-
-const ButtonBlockFinish = ({ size, buttonStart, buttonStep }) => {
+const ButtonBlockFinish = ({ size, buttonStart, buttonFinish }) => {
 
   let arrayButton = []
   for (let i = 1; i <= Math.pow(size, 2); i++) {
     arrayButton[i] = i
   }
 
+  const [s, setS] = useState(false)
 
   return (
-    <div className='App' >
-      {
-        arrayButton.map((arrayButton, i) =>
-          (arrayButton === buttonStep && buttonStep === buttonStart &&
+    <div >
+      {!s &&
+        <button className='finish' onClick={() => setS(s => !s)}>
+          Показать кнопку финиш
+        </button>
+      }
+      <div className='App' >
+        {s && arrayButton.map((arrayButton, i) =>
+          (arrayButton === buttonFinish && buttonFinish === buttonStart &&
             <button key={i} className='buttonStartFinish'>
-              <div >Start-Finish</div>
+              <div >Старт и Финиш</div>
             </button>) ||
           (arrayButton === buttonStart && <button key={i} className='buttonStart' >Start</button>) ||
-          (arrayButton === buttonStep && <button key={i} className='buttonFinish'>Finish</button>) ||
+          (arrayButton === buttonFinish && <button key={i} className='buttonFinish'>Finish</button>) ||
           (<button key={i} className='button'>{arrayButton}</button>)
-        )
-      }
+        )}
+      </div >
     </div >
   )
 }
